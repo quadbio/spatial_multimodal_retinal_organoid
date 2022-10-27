222710fac0b1a34180479fae598de657418654fc
Philipp Wahle <philipp.wahle@bsse.ethz.ch>
2022-10-27 09:54:58 +0200

    cleaned paths in fSOM

--- .ipynb_checkpoints/fSOM-checkpoint.R
+++ .ipynb_checkpoints/fSOM-checkpoint.R
@@ -1,5 +1,3 @@
-data.path = "/links/groups/treutlein/DATA/imaging/charmel/publication_example_data_2/"
-proj.path = '/home/pwahle/4i_publication_repo_2'
 library(reticulate)
 use_condaenv("conda_3.7.9", required=TRUE)
 Sys.unsetenv("LD_LIBRARY_PATH")
@@ -17,8 +15,8 @@ if (!file.exists(paste0(data.path, '/fSOM_output/fSOM.rds'))){
 pixel_matrix <- np$load(paste0(data.path, 'pixel_matrices/scaled_fusion_sub250_z_normalised_pixel_matrix.npz'))
 pixel_matrix = pixel_matrix$f[["arr_0"]]
 
-yamlpath = '/home/pwahle/4i_publication_repo_2/metadata.yml'
-colnames = read_yaml(yamlpath)
+colnames = read_yaml('metadata.yml')
+params = read_yaml('params.yml')
 
 colnames(pixel_matrix) = colnames
 
@@ -38,11 +36,11 @@ fSOM = FlowSOM::ReadInput(data_FlowSOM, transform = FALSE, scale = FALSE)
 fSOM = FlowSOM::BuildSOM(fSOM, colsToUse = marker_cols, ydim = ydim_SOM, xdim = xdim_SOM, distf = dist_metric, rlen = num_runs)
 
 dir.create(paste0(data.path, '/fSOM_output/'), recursive = TRUE)
-saveRDS(fSOM, file = paste0(data.path, '/fSOM_output/fSOM.rds'))
+saveRDS(fSOM, file = paste0(params$data_path, '/fSOM_output/fSOM.rds'))
 som_codes = fSOM$map$codes
-np$savez_compressed(paste0(data.path, '/fSOM_output/som_codes.npz'), som_codes)
+np$savez_compressed(paste0(params$data_path, '/fSOM_output/som_codes.npz'), som_codes)
 } else {
-  fSOM = readRDS(paste0(data.path, '/fSOM_output/fSOM.rds'))
+  fSOM = readRDS(paste0(params$data_path, '/fSOM_output/fSOM.rds'))
   som_codes = fSOM$map$codes
 }
 
@@ -68,14 +66,14 @@ print(detected$MinIndex)
 df = data.frame(neighs = rev(neigh_to_check), clusts = rev(unlist(n_clusters_detected)))
 unlist(n_clusters_detected)[15]
 
-png(file=paste0(data.path, '/fSOM_output/kneeplot.png'), width=600, height=350)
+png(file=paste0(params$data_path, '/fSOM_output/kneeplot.png'), width=600, height=350)
 plot(rev(unlist(n_clusters_detected)), rev(neigh_to_check), xaxp = c(0, 200, 20))
 abline(v = df[detected$MinIndex, 2], col = "blue", lwd = c(1, 3))
 abline(h = df[detected$MinIndex, 1], col = 'red', lwd = c(1, 3))
 dev.off()
 
 print(df)
-print(paste0('Choose k nearest neighbors to use for phenograph fom kneeplot in ', data.path, '/fSOM_output. Or choose from table printed above, or use default of 15 neighbors to get 31 MTUs'))
+print(paste0('Choose k nearest neighbors to use for phenograph fom kneeplot in ', params$data_path, '/fSOM_output. Or choose from table printed above, or use default of 15 neighbors to get 31 MTUs'))
 
 
 

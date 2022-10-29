52107597b29a117878e5c88fa823114050f6f0b0
Philipp Wahle <philipp.wahle@bsse.ethz.ch>
2022-10-29 21:20:46 +0200

    bugfix

--- .ipynb_checkpoints/fSOM-checkpoint.R
+++ .ipynb_checkpoints/fSOM-checkpoint.R
@@ -39,12 +39,12 @@ set.seed(1234)
 fSOM = FlowSOM::ReadInput(data_FlowSOM, transform = FALSE, scale = FALSE)
 fSOM = FlowSOM::BuildSOM(fSOM, colsToUse = marker_cols, ydim = ydim_SOM, xdim = xdim_SOM, distf = dist_metric, rlen = num_runs)
 
-dir.create(paste0(params$data.path, '/fSOM_output/'), recursive = TRUE)
-saveRDS(fSOM, file = paste0(params$data_path, '/fSOM_output/fSOM.rds'))
+#dir.create(paste0(params$data.path, '/fSOM_output/'), recursive = TRUE)
+saveRDS(fSOM, file = 'fSOM.rds')
 som_codes = fSOM$map$codes
-np$savez_compressed(paste0(params$data_path, '/fSOM_output/som_codes.npz'), som_codes)
+np$savez_compressed('som_codes.npz', som_codes)
 } else {
-  fSOM = readRDS(paste0(params$data_path, '/fSOM_output/fSOM.rds'))
+  fSOM = readRDS('fSOM.rds'))
   som_codes = fSOM$map$codes
 }
 
@@ -70,7 +70,7 @@ print(detected$MinIndex)
 df = data.frame(neighs = rev(neigh_to_check), clusts = rev(unlist(n_clusters_detected)))
 unlist(n_clusters_detected)[15]
 
-png(file=paste0(params$data_path, '/fSOM_output/kneeplot.png'), width=600, height=350)
+png(file='kneeplot.png'), width=600, height=350)
 plot(rev(unlist(n_clusters_detected)), rev(neigh_to_check), xaxp = c(0, 200, 20))
 abline(v = df[detected$MinIndex, 2], col = "blue", lwd = c(1, 3))
 abline(h = df[detected$MinIndex, 1], col = 'red', lwd = c(1, 3))

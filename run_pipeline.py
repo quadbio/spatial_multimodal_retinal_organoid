132ef17bddf57e115207d2b3d9336856046f4dcc
Christoph Harmel <christoph.harmel@unibas.ch>
2022-12-21 15:07:15 +0100

    fix: refactored modules.py, updated .gitignore

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,109 +1,105 @@
-
-import yaml 
+import yaml
 import importlib
-import modules 
+import modules
 from time import gmtime, strftime
 import os
 from pathlib import Path
 import subprocess
 
-#load global variables and parameters
-with open("params.yml", 'r') as ymlfile: 
-   cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+# load global variables and parameters
+with open("params.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
 globals().update(cfg)
-r_path = str(Path('/usr/local/R-4.0.3/bin/Rscript')) # path to r interpreter
-#################-----------------------------------------------------------------########################
+r_path = str(Path('/usr/local/R-4.0.3/bin/Rscript'))  # path to r interpreter
+
+# run pipeline
 for well in wells:
     print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
     print('simple masking sample:' + str(well))
     modules.simple_mask(point=well, cycle=ref_cycles[0],
-                dir_input=data_path,
-                dir_output=data_path + 'masks/',
-                save=True,
-                plot=False,
-                show_plot=False,
-                save_plot=True)
+                        dir_input=data_path,
+                        dir_output=data_path + 'masks/',
+                        save=True,
+                        plot=False,
+                        show_plot=False,
+                        save_plot=True)
 
-    
-    img_df = modules.get_metadata(Path(data_path,'raw_data'))
+    img_df = modules.get_metadata(Path(data_path, 'raw_data'))
     filenames = img_df.loc[(img_df['well_id'] == well)]['file']
-    
+
     items = []
     for i in filenames:
-        items.append(Path(data_path,'aligned', i)) 
-    
-    print('aligning well '+ str(well))
+        items.append(Path(data_path, 'aligned', i))
+
+    print('aligning well ' + str(well))
     if not (all(os.path.isfile(i) for i in items)):
-        modules.run_elastix(point = well,
-                    ref_cycles = ref_cycles,
-                    dir_mask = Path(data_path,'masks'),
-                    dir_input = Path(data_path,'raw_data'),
-                    dir_output = Path(data_path,'aligned'))
+        modules.run_elastix(point=well,
+                            ref_cycles=ref_cycles,
+                            dir_mask=Path(data_path, 'masks'),
+                            dir_input=Path(data_path, 'raw_data'),
+                            dir_output=Path(data_path, 'aligned'))
     else:
         print('well ' + str(well) + ' already aligned')
 
-    print('denoising well '+ str(well))
-    modules.denoise_well(well = well)
-    
-    
-    print('refigning mask well '+ str(well))
-    modules.refine_mask(well = well)
-    
-    
-    img_df = modules.get_metadata(Path(data_path,'denoised'))
+    print('denoising well ' + str(well))
+    modules.denoise_well(well=well)
+
+    print('refigning mask well ' + str(well))
+    modules.refine_mask(well=well)
+
+    img_df = modules.get_metadata(Path(data_path, 'denoised'))
     filenames = img_df.loc[(img_df['well_id'] == well)]['file']
-    
+
     items = []
     for i in filenames:
-        items.append(Path(data_path,'bg_subtracted', i)) 
-        
-    print('subtracting backgrounds well '+ str(well))
-    if not (all(os.path.isfile(i) for i in items)):
-        modules.bg_subtraction(well = well,
-                        dir_input=Path(data_path,'denoised'),
-                        dir_masks=Path(data_path,'masks'),
-                        dir_output=Path(data_path,'bg_subtracted'),
-                        cycles_bg=[0, 6, 12],
-                        cycle_hoechst = 1)
-    
-    print('segmenting nuclei well '+ str(well))
-    modules.segment_nuclei(well = well, 
-                           ref_cycle = 0, 
-                           dir_input = Path(data_path, 'denoised'), 
-                           dir_output = Path(data_path, 'segmented_nuclei'))
-   
-    print('generating pixel matrix well '+ str(well))
-    modules.generate_pixel_matrices(well = well,
-                         dir_input = Path(data_path,"bg_subtracted"),
-                         dir_output = Path(data_path,'pixel_matrices/'),
-                         cycles_bg = cycles_bg,
-                         to_collagen_mask = to_collagen_mask,
-                         mask_collagen = False,
-                         overwrite_denoised_images_with_collagen_masked_images = False)
+        items.append(Path(data_path, 'bg_subtracted', i))
 
-conditions = {'week39' : [43,44]}
+    print('subtracting backgrounds well ' + str(well))
+    if not (all(os.path.isfile(i) for i in items)):
+        modules.bg_subtraction(well=well,
+                               dir_input=Path(data_path, 'denoised'),
+                               dir_masks=Path(data_path, 'masks'),
+                               dir_output=Path(data_path, 'bg_subtracted'),
+                               cycles_bg=[0, 6, 12],
+                               cycle_hoechst=1)
+
+    print('segmenting nuclei well ' + str(well))
+    modules.segment_nuclei(well=well,
+                           ref_cycle=0,
+                           dir_input=Path(data_path, 'denoised'),
+                           dir_output=Path(data_path, 'segmented_nuclei'))
+
+    print('generating pixel matrix well ' + str(well))
+    modules.generate_pixel_matrices(well=well,
+                                    dir_input=Path(data_path, "bg_subtracted"),
+                                    dir_output=Path(data_path, 'pixel_matrices/'),
+                                    cycles_bg=cycles_bg,
+                                    to_collagen_mask=to_collagen_mask,
+                                    mask_collagen=False,
+                                    overwrite_denoised_images_with_collagen_masked_images=False)
+
+conditions = {'week39': [43, 44]}
 for condition in conditions:
-    modules.run_matrix_normalization(wells, 
-                             dir_input = Path(data_path, 'pixel_matrices'),
-                             dir_output = Path(data_path,'pixel_matrices'), 
-                             fusion_matrix = True)
+    modules.run_matrix_normalization(wells,
+                                     dir_input=Path(data_path, 'pixel_matrices'),
+                                     dir_output=Path(data_path, 'pixel_matrices'),
+                                     fusion_matrix=True)
 
 os.makedirs(Path(data_path, 'fSOM_output'), exist_ok=True)
 if not os.path.isfile(Path(data_path, 'fSOM_output/fSOM.rds')):
-                      subprocess.run([r_path,'fSOM.R'])
+    subprocess.run([r_path, 'fSOM.R'])
 
 for well in wells:
-    modules.MTU_assignment(well = well, 
-                   k = 20, 
-                   dir_matrix = Path(data_path, 'pixel_matrices'),
-                   dir_output = Path(data_path, 'MTU_results'),
-                   dir_fSOM = Path(data_path, 'fSOM_output'))
+    modules.MTU_assignment(well=well,
+                           k=20,
+                           dir_matrix=Path(data_path, 'pixel_matrices'),
+                           dir_output=Path(data_path, 'MTU_results'),
+                           dir_fSOM=Path(data_path, 'fSOM_output'))
 
 for well in wells:
-    modules.run_nuclear_features_table(well = well,
-                             dir_input = Path(data_path,'bg_subtracted'),
-                             dir_nuclei = Path(data_path,'segmented_nuclei'),
-                             dir_MTU_img = Path(data_path, 'MTU_results'),
-                             dir_output = Path(data_path,'feature_tables'),
-                             )
+    modules.run_nuclear_features_table(well=well,
+                                       dir_input=Path(data_path, 'bg_subtracted'),
+                                       dir_nuclei=Path(data_path, 'segmented_nuclei'),
+                                       dir_MTU_img=Path(data_path, 'MTU_results'),
+                                       dir_output=Path(data_path, 'feature_tables'))
